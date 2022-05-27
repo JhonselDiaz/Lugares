d9d3f27d6a20427e98e8017d6fc889184c6a83b9
Jhonsel Diaz <jdiazch.21@gmail.com>
2022-05-26 22:59:48 -0600

    Semana 2

--- app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
@@ -1,4 +1,4 @@
-package com.lugares.ui.gallery
+package com.lugares.ui.lugar
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.lugares.databinding.FragmentGalleryBinding
-import com.lugares.viewmodel.GalleryViewModel
+import com.lugares.databinding.FragmentLugarBinding
+import com.lugares.viewmodel.LugarViewModel
 
-class GalleryFragment : Fragment() {
+class LugarFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentLugarBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,14 +23,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val homeViewModel =
+            ViewModelProvider(this).get(LugarViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentLugarBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
